15b6aee5708f261e36c02471dae0f9143a506be3
Alireza Ahmadi (Arma) <alirezama@hotmail.com>
2025-03-24 07:00:46 +0330

    Remove unnecessary comments

--- wp-content/plugins/iranian-league-table/includes/iranianleaguetable-scripts.php
+++ wp-content/plugins/iranian-league-table/includes/iranianleaguetable-scripts.php
@@ -1,14 +1,9 @@
 <?php
-  // Add Scripts
-  function yts_add_scripts(){
-    // Add Main CSS
-    wp_enqueue_style('yts-main-style', plugins_url(). '/iranian-league-table/css/style.css');
-    // Add Main JS
-    wp_enqueue_script('yts-main-script', plugins_url(). '/iranian-league-table/js/main.js', NULL, NULL, true);
 
-    // Add Google Script
-    // wp_register_script('varzesh3', 'https://static.varzesh3.com/js/developers-varzesh3.js');
-    // wp_enqueue_script('varzesh3');
+  function yts_add_scripts(){
+    wp_enqueue_style('yts-main-style', plugins_url(). '/iranian-league-table/css/style.css');
+    wp_enqueue_script('yts-main-script', plugins_url(). '/iranian-league-table/js/main.js', NULL, NULL, true);
   }
 
   add_action('wp_enqueue_scripts', 'yts_add_scripts');
+  
