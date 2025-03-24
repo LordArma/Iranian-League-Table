8b75f9a6713c1512038d7ff67ddc05058d177957
Alireza Ahmadi (Arma) <alirezama@hotmail.com>
2025-03-24 06:55:00 +0330

    Remove unnecessary comments

--- wp-content/plugins/iranian-league-table/iranianleaguetable.php
+++ wp-content/plugins/iranian-league-table/iranianleaguetable.php
@@ -10,21 +10,15 @@ Text Domain: ilt_domain
 Domain Path: /languages
 */
 
-// Exit if accessed directly
 if(!defined('ABSPATH')){
   exit;
 }
 
-// Load Scripts
 require_once(plugin_dir_path(__FILE__).'/includes/iranianleaguetable-scripts.php');
-
-// Load Class
 require_once(plugin_dir_path(__FILE__).'/includes/iranianleaguetable-class.php');
 
-// Register Widget
 function register_iranian_league(){
   register_widget('Iranian_League_Widget');
 }
 
-// Hook in function
 add_action('widgets_init', 'register_iranian_league');
